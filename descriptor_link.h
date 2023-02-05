20937ecc1bebbc42a7458b46890d6b6d941697ea
pika77o <87273975+pica770@users.noreply.github.com>
2023-02-05 15:47:04 +0300

    Update descriptor_link.h

--- descriptor_link.h
+++ descriptor_link.h
@@ -1,5 +1,27 @@
-void descriptor_link::show_info()
+#pragma once
+#include "descriptor.h"
+#include <string>
+#include "type_protocol.h"
+using namespace std;
+
+class descriptor_link :
+    public descriptor
 {
-	string type_p = m_protokol == type_protokol::ftp ? "FTP" : "Http";
-	cout << "Тип протокола: " << type_p << ", Имя сервера: " << m_server_name << endl;
-}
+private:
+    type_protokol m_protokol;
+    string m_server_name;
+
+public:
+    descriptor_link(type_protokol protokol, string server_name) : m_protokol(protokol), m_server_name(server_name) {}
+
+    type_protokol get_type_protokol() {
+        return m_protokol;
+    }
+
+    string get_server_name() {
+        return m_server_name;
+    }
+
+    void show_info() override;
+};
+
