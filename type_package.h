b023fe1e0576f51c614aee25abf33f3dc8ba432c
pika77o <87273975+pica770@users.noreply.github.com>
2023-02-05 15:53:48 +0300

    Update type_package.h

--- type_package.h
+++ type_package.h
@@ -1,26 +1,5 @@
 #pragma once
-#include "package_descriptor.h"
-#include <vector>
-#include <set>
 
-class server
-{
-private:
-	string m_name;
-	string m_address;
-	vector<package_descriptor*> m_packages;
-
-public:
-	server(string name, string address) : m_name(name), m_address(address) {}
-
-	void add(package_descriptor* package);
-	package_descriptor* find_package(string address_recipient, type_package type);
-	bool remove_package(string address_recipient, type_package type);
-
-	string get_name() const;
-	string get_address() const;
-
-	void show();
-	void show_senders();
+enum class type_package {
+	mail, file, hypertext
 };
-
