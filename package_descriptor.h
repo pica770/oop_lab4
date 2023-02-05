0f3b41b2731a3944c2c756a5dafc47637a3949da
pika77o <87273975+pica770@users.noreply.github.com>
2023-02-05 15:49:59 +0300

    Update package_descriptor.h

--- package_descriptor.h
+++ package_descriptor.h
@@ -1,7 +1,33 @@
-void package_descriptor::show_info()
+#pragma once
+#include <iostream>
+#include "descriptor_message.h"
+#include "type_package.h"
+using namespace std;
+
+class package_descriptor
 {
-	cout << "Адрес отправителя: " << m_address_sender << endl;
-	cout << "Адрес получателя: " << m_address_recipient << endl;
-	cout << "Описатель сообщения: ";
-	m_descriptor_message->show_info();
-}
+private:
+	string m_address_sender;
+	string m_address_recipient;
+	descriptor_message* m_descriptor_message;
+
+public:
+	package_descriptor(string address_sender, string address_recipient, descriptor_message* descriptor_message) :
+		m_address_sender(address_sender), m_address_recipient(address_recipient), m_descriptor_message(descriptor_message) {}
+
+	string get_address_sender() {
+		return m_address_sender;
+	}
+
+	string get_address_recipient() {
+		return m_address_recipient;
+	}
+
+	descriptor_message* get_descriptor_message() {
+		return m_descriptor_message;
+	}
+
+	virtual type_package get_type_package() = 0;
+	virtual void show_info();
+};
+
