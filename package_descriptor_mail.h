19dc47af930bd8623d2fd128b883532cec957001
pika77o <87273975+pica770@users.noreply.github.com>
2023-02-05 15:52:20 +0300

    Update package_descriptor_mail.h

--- package_descriptor_mail.h
+++ package_descriptor_mail.h
@@ -1,10 +1,28 @@
-void package_descriptor_mail::show_info()
-{
-	package_descriptor::show_info();
-	cout << "Имя пользователя: " << m_user_name << endl;
-}
+#pragma once
+#include "package_descriptor.h"
+#include "package_descriptor_file.h"
 
-package_descriptor_file* package_descriptor_mail::convert(type_code t_code, info_type t_info)
+class package_descriptor_mail :
+    public package_descriptor
 {
-	return new package_descriptor_file(get_address_sender(), get_address_recipient(), get_descriptor_message(), t_code, t_info);
-}
+private:
+    string m_user_name;
+
+public:
+    package_descriptor_mail(string address_sender, string address_recipient, descriptor_message* descriptor_message,
+        string user_name) : package_descriptor(address_sender, address_recipient, descriptor_message), m_user_name(user_name) {
+    }
+
+    string get_user_name() {
+        return m_user_name;
+    }
+
+    type_package get_type_package() override {
+        return type_package::mail;
+    }
+
+    void show_info() override;
+
+    package_descriptor_file* convert(type_code t_code, info_type t_info);
+};
+
